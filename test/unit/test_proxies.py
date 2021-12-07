d51963a808b1dd8be49b4bb1158491c7382a8e31
Nate Harris <nwithan8@users.noreply.github.com>
2021-12-07 01:24:21 -0700

    Unit test for proxy_dict

--- test/unit/test_proxies.py
+++ test/unit/test_proxies.py
@@ -11,3 +11,16 @@ def test_list_proxies():
     proxy_list = proxlist.list_proxies()
 
     assert proxy_list == proxlist.proxies.PROXY_LIST
+
+    
+def test_proxy_dict():
+    proxy_dict = proxlist.proxy_dict()
+    http = proxy_dict.get('http', None)
+    https = proxy_dict.get('https', None)
+
+
+    assert proxy_dict is not None
+    assert http is not None
+    assert http.startswith('http://')
+    assert https is not None
+    assert https.startswith('https://')
