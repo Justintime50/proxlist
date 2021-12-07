66788f4d8ea9a87c32ec1eca27bff9262e968448
Nate Harris <nwithan8@users.noreply.github.com>
2021-12-07 01:26:49 -0700

    Add proxy_dict to __all__

--- proxlist/__init__.py
+++ proxlist/__init__.py
@@ -1,6 +1,7 @@
-from proxlist.proxies import list_proxies, random_proxy
+from proxlist.proxies import list_proxies, random_proxy, proxy_dict
 
 __all__ = [
     'random_proxy',
     'list_proxies',
+    'proxy_dict'
 ]
